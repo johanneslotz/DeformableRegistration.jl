7e0b5e3da006952717ce2420d3657da17d17bc11
Nick <nickw@web.de>
2014-12-17 09:05:57 +0100

    chnagend nonparametric registration slightly

--- test/NonparametricRegistration.jl
+++ test/NonparametricRegistration.jl
@@ -11,19 +11,15 @@ temImg = createImage(data) 
 data[41:80,41:90] = 1
 refImg = createImage(data)
 
-print(refImg["spatialdomain"])
-print(refImg["pixelspacing"])
-print(size(refImg),"\n")
-
 # register images nonparametric
-deformationField = registerImagesNonparametric(refImg,temImg)
+deformationField = registerImagesNonparametric(refImg,temImg,alpha=0.5)
 #@test_approx_eq_eps
-ssdvalue = ssdDistance(refImg,temImg,getCellCenteredGrid(refImg)+deformationField)[1] 
+ssdvalue = ssdDistance(refImg,temImg,getCellCenteredGrid(refImg)+deformationField)[1]
 
 println("The recursion test is temporarily disabled, SSD value after nonlinear registration:")
 println(ssdvalue)
 
 # visualize results
-using PyPlot; pygui(true); close("all")
-figure()
-visualizeResults(refImg,temImg,deformationField=deformationField)
+# using PyPlot; pygui(true); close("all")
+# figure()
+# visualizeResults(refImg,temImg,deformationField=deformationField)
