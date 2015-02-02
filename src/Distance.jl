c3fec9b8ddd90d8b2fff21c91a06ce72f636a76a
Johannes Lotz <jo@joip.de>
2015-02-02 19:11:51 +0100

    added function to estimate ngf epsilon

--- src/Distance.jl
+++ src/Distance.jl
@@ -2,13 +2,15 @@ module Distance
 
 using Images
 using ImageRegistration.Transformation
+import Logging
 
-export ssdDistance, ssdDistanceMatrixFree, maskedSsdDistance, ngfDistance
+export ssdDistance, ssdDistanceMatrixFree, maskedSsdDistance, ngfDistance, estimateNGFEpsilon
 export calculateHessianStaggered
 
 include("distances/ssdDistance.jl")
 include("distances/maskedSsdDistance.jl")
 include("distances/ngfDistance.jl")
+include("distances/estimateNGFEpsilon.jl")
 
 
 end
