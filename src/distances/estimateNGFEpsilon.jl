d70c59bb612329686e1c1e88663941ffae0c3cd2
Johannes Lotz <jo@joip.de>
2015-02-03 15:23:33 +0100

    estimate ngf epsilon based on edge image

--- src/distances/estimateNGFEpsilon.jl
+++ src/distances/estimateNGFEpsilon.jl
@@ -1,43 +1,62 @@
+using ImageRegistration
 using ImageRegistration.ImageProcessing
-using ImageRegistration.Transformation
-using ImageRegistration.Distance
+using ImageRegistration.Visualization
 using Images
 using Stats
+using PyPlot
+import Logging
 
+function estimateNGFEpsilon(referenceImage::Image;cutoffPercent = 80)
+  m = size(referenceImage)
+  h = referenceImage.properties["pixelspacing"]
+  Rc = referenceImage.data
+  ΩR = referenceImage.properties["spatialdomain"]
 
-function estimateNGFEpsilon(referenceImage::Image)
+  shortFiniteDiffX = spdiagm((-ones(m[1]-1,1),ones(m[1]-2,1)),[0, 1],m[1]-1,m[1])/(2*h[1])
+  #shortFiniteDiffX[1] = shortFiniteDiffX[2]
+  #shortFiniteDiffX[end] = shortFiniteDiffX[end-1]
 
-    m = size(referenceImage)
-    h = referenceImage.properties["pixelspacing"]
-    Rc = referenceImage.data
-    ΩR = referenceImage.properties["spatialdomain"]
+  shortFiniteDiffY = spdiagm((-ones(m[2]-1,1),ones(m[2]-2,1)),[0, 1],m[2]-1,m[2])/(2*h[2])
+  #shortFiniteDiffY[1] = shortFiniteDiffY[2]
+  #shortFiniteDiffY[end] = shortFiniteDiffY[end-1]
 
-    shortFiniteDiffX = spdiagm((-ones(m[1]-1,1),ones(m[1]-2,1)),[0, 1],m[1]-1,m[1])/(2*h[1])
-    #shortFiniteDiffX[1] = shortFiniteDiffX[2]
-    #shortFiniteDiffX[end] = shortFiniteDiffX[end-1]
+  averageX = spdiagm((0.5*ones(m[1]-2,1),0.5*ones(m[1]-1,1)),[-1, 0],m[1],m[1]-1)
+  averageY = spdiagm((ones(m[2]-2,1)*0.5,ones(m[2]-1,1)*0.5),[-1, 0],m[2],m[2]-1)
 
-    shortFiniteDiffY = spdiagm((-ones(m[2]-1,1),ones(m[2]-2,1)),[0, 1],m[2]-1,m[2])/(2*h[2])
-    #shortFiniteDiffY[1] = shortFiniteDiffY[2]
-    #shortFiniteDiffY[end] = shortFiniteDiffY[end-1]
+  G1 = sparse(kron(speye(m[2]),shortFiniteDiffX));
+  G2 = sparse(kron(shortFiniteDiffY,speye(m[1])));
 
-    averageX = spdiagm((0.5*ones(m[1]-2,1),0.5*ones(m[1]-1,1)),[-1, 0],m[1],m[1]-1)
-    averageY = spdiagm((ones(m[2]-2,1)*0.5,ones(m[2]-1,1)*0.5),[-1, 0],m[2],m[2]-1)
+  AvgX = sparse(kron(speye(m[2]),averageX));
+  AvgY = sparse(kron(averageY,speye(m[1])));
 
-    G1 = sparse(kron(speye(m[2]),shortFiniteDiffX));
-    G2 = sparse(kron(shortFiniteDiffY,speye(m[1])));
+  gradTx  = G1*referenceImage[:]
+  gradTy  = G2*referenceImage[:]
 
-    AvgX = sparse(kron(speye(m[2]),averageX));
-    AvgY = sparse(kron(averageY,speye(m[1])));
+  quantileX = percentile(AvgX * (gradTx.^2),cutoffPercent)
+  quantileY = percentile(AvgY * (gradTy.^2),cutoffPercent)
 
-    gradTx  = G1*referenceImage[:]
-    gradTy  = G2*referenceImage[:]
+  estimatedEps = sqrt(0.5*(quantileX + quantileY))
 
-    quantileX = percentile(AvgX * (gradTx.^2),95)
-    quantileY = percentile(AvgY * (gradTy.^2),95)
+  lengthGT = sqrt(sum(AvgX * (gradTx.^2) + AvgY * (gradTy.^2), 2) + estimatedEps^2) #epsilon-norm
 
-    estimatedEps = sqrt(0.5*(quantileX + quantileY))
+  vectorlength = reshape(lengthGT,(size(referenceImage)))
 
-    return estimatedEps
+  try
+    figure(51)
+    clf()
+    showImage(vectorlength)
+    s = @sprintf("estimated epsilon: %3.3e", estimatedEps)
+    title(s)
+    colorbar()
+    figure(52)
+    clf()
+    plt.hist(lengthGT,bins=100)
+  catch err
+    Logging.warn("Error while plotting. This is normal if it happens in the automated test framework.")
+    Logging.warn(err)
+  end
+
+  return (estimatedEps,vectorlength)
 
 
 end
