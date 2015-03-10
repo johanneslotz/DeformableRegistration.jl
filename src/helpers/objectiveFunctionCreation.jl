0c74c19f5f8e2ccd8acf26a6b7c6a892998acd31
Johannes Lotz <jo@joip.de>
2015-03-10 14:49:31 +0100

    added overloaded + operator matching ngf signature

--- src/helpers/objectiveFunctionCreation.jl
+++ src/helpers/objectiveFunctionCreation.jl
@@ -8,7 +8,16 @@ function +(Distance::(Number,Number,Number,(Vector,Vector)),Regularizer::(Number
     return Distance[1].+Regularizer[1], 0, 0
 end
 
+function +(Distance::(Number,Number,Number),Regularizer::(Number,Vector,SparseMatrixCSC{Float64,Int64}))
+    return Distance[1].+Regularizer[1], 0, 0
+end
+
 function +(Distance::(Number,Vector,Function,(Vector,Vector)),Regularizer::(Number,Vector,SparseMatrixCSC{Float64,Int64}))
+    d2J(grid) = Distance[3](grid) + Regularizer[3]*grid
+    return Distance[1].+Regularizer[1], Distance[2].+Regularizer[2], d2J
+end
+
+function +(Distance::(Number,Vector,Function),Regularizer::(Number,Vector,SparseMatrixCSC{Float64,Int64}))
     d2J(grid) = Distance[3](grid) + Regularizer[3]*grid
     return Distance[1].+Regularizer[1], Distance[2].+Regularizer[2], d2J
 end
