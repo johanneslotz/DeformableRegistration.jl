9dc4472a71f28957b14fa8f437e534ad6eb81800
jo <jo@mail.x91.de>
2017-09-22 14:06:13 +0200

    fixed Tuple-datatype

--- src/helpers/objectiveFunctionCreation.jl
+++ src/helpers/objectiveFunctionCreation.jl
@@ -1,29 +1,27 @@
 # helpers to make addition/multiplication of distance and regularizer possible
 
-function *(factor::Number,Regularizer::(Number,Vector,SparseMatrixCSC{Float64,Int64}))
+function *(factor::Number,Regularizer::Tuple{Number,Vector,SparseMatrixCSC{Float64,Int64}})
     return factor.*Regularizer[1], factor.*Regularizer[2], factor.*Regularizer[3]
 end
 
-function +(Distance::(Number,Number,Number,(Vector,Vector)),Regularizer::(Number,Vector,SparseMatrixCSC{Float64,Int64}))
+function +(Distance::Tuple{Number,Number,Number,Tuple{Vector,Vector}},Regularizer::Tuple{Number,Vector,SparseMatrixCSC{Float64,Int64}})
     return Distance[1].+Regularizer[1], 0, 0
 end
 
-function +(Distance::(Number,Number,Number),Regularizer::(Number,Vector,SparseMatrixCSC{Float64,Int64}))
+function +(Distance::Tuple{Number,Number,Number},Regularizer::Tuple{Number,Vector,SparseMatrixCSC{Float64,Int64}})
     return Distance[1].+Regularizer[1], 0, 0
 end
 
-function +(Distance::(Number,Vector,Function,(Vector,Vector)),Regularizer::(Number,Vector,SparseMatrixCSC{Float64,Int64}))
+function +(Distance::Tuple{Number,Vector,Function,Tuple{Vector,Vector}},Regularizer::Tuple{Number,Vector,SparseMatrixCSC{Float64,Int64}})
     d2J(grid) = Distance[3](grid) + Regularizer[3]*grid
     return Distance[1].+Regularizer[1], Distance[2].+Regularizer[2], d2J
 end
 
-function +(Distance::(Number,Vector,Function),Regularizer::(Number,Vector,SparseMatrixCSC{Float64,Int64}))
+function +(Distance::Tuple{Number,Vector,Function},Regularizer::Tuple{Number,Vector,SparseMatrixCSC{Float64,Int64}})
     d2J(grid) = Distance[3](grid) + Regularizer[3]*grid
     return Distance[1].+Regularizer[1], Distance[2].+Regularizer[2], d2J
 end
 
-function +(Distance::(Number,Vector,SparseMatrixCSC{Float64,Int64},(Vector,Vector)),Regularizer::(Number,Vector,SparseMatrixCSC{Float64,Int64}))
+function +(Distance::Tuple{Number,Vector,SparseMatrixCSC{Float64,Int64},Tuple{Vector,Vector}},Regularizer::Tuple{Number,Vector,SparseMatrixCSC{Float64,Int64}})
     return Distance[1].+Regularizer[1], Distance[2].+Regularizer[2], Distance[3].+Regularizer[3]
 end
-
-
