9e41b48b6e9ed5eff030c278d733d02c695d53a7
Johannes Lotz <jo+github@x91.de>
2018-02-01 15:11:17 +0100

    added some experiments

---
+++ experimental/experiment-fixedGrid.jl
@@ -0,0 +1,97 @@
+using Images
+ using Logging
+ using DeformableRegistration: Visualization, Examples, ImageProcessing, Transformation, Interpolation, Distance
+ using DeformableRegistration.Regularizer
+ using DeformableRegistration.regOptions
+ using Base.Test
+ Logging.configure(level=Logging.WARNING)
+ include("./constraint.jl")
+ include("./test-helpers.jl")
+ include("./fixedGridRegistration.jl")
+
+plot=true
+showDeformationImage = false
+function testFixedGridRegistration(;plot=true,showDeformationImage=false)
+
+refImg, temImg, options = constructTestImages()
+    options.stopping["tolQ"] = 1e-10
+    options.stopping["tolJ"] = 1e-10
+    options.stopping["tolY"] = 1e-10
+    options.stopping["tolG"] = 1e-10
+
+    reg=createDiffusiveOperatorCentered
+
+    #interpolationScheme=BSpline(Linear())
+    interpolationScheme=InterpLinearFast
+
+    m = getCellCenteredGrid(refImg).dimensions
+    B = createDiffusiveOperatorCentered([1.0,1.0],[m[1],m[2]])
+    B=B'*B
+
+    options.interpolateToReferenceImage = true
+
+    options.maxIterCG = 10000
+    plotRefImg, plotTemImg, plotOptions = constructTestImages()
+    plotTemImg.data[13:12:end,13:12:end] = 1
+    plotTemImg.data[14:12:end,13:12:end] = 1
+    plotTemImg.data[13:12:end,14:12:end] = 1
+    plotTemImg.data[14:12:end,14:12:end] = 1
+
+    #"FIXED GRID RESOLUTION"
+    close("all")
+options.levels = [4, 3, 2, 1, 0]
+    options.regularizerWeight = 0.8
+    @time singleGridDisplacement = registerNonParametricFixedGridResolution(refImg, temImg, options,
+                                    regularizerOperator=reg, interpolationScheme=interpolationScheme, gridLevel=2) # regularizerOperator=createDiffusiveOperatorCentered
+    plot ? (figure();  visualizeResults(refImg, plotTemImg, displacement= singleGridDisplacement, showDeformationImage=showDeformationImage, suptitle="fixed grid resolution at level", filename = "");) : 0
+    ssdCoarse = ssdDistance(refImg, temImg, (singleGridDisplacement+getCellCenteredGrid(refImg)).data, doDerivative=true)[1]
+    regularizerCoarse = singleGridDisplacement.data[:]'*B*singleGridDisplacement.data[:]
+
+
+    #"ALL MULTILEVEL"
+options.levels = [4, 3, 2, 1]
+    options.regularizerWeight = 1
+    @time fineDisplacement = registerNonParametricConstraint(refImg, temImg, options,regularizerOperator=reg, interpolationScheme=interpolationScheme) # regularizerOperator=createDiffusiveOperatorCentered
+    plot ? (figure();  visualizeResults(refImg, plotTemImg, displacement= fineDisplacement, showDeformationImage=showDeformationImage, suptitle="Fine full registration", filename = "");) : 0
+    ssdFine = ssdDistance(refImg, temImg, (fineDisplacement+getCellCenteredGrid(refImg)).data, doDerivative=true)[1]
+    regularizerFine = fineDisplacement.data[:]'*B*fineDisplacement.data[:]
+
+
+@printf("\n")
+
+    @printf("\n
+    SSD values
+    ---------------------------------
+    single grid:           %3.3e
+    fine full registration %3.3e\n", ssdCoarse, ssdFine)
+
+
+    @printf("\n
+    error norm (y-y_fine)
+    ---------------------------------
+    single Grid:           %3.3e
+    fine full registration %3.3e\n",
+    norm(singleGridDisplacement.data - fineDisplacement.data),
+    norm(fineDisplacement.data - fineDisplacement.data))
+
+
+    @printf("\n
+    REGULARIZER values
+    ---------------------------------
+    single Grid           %3.3e
+    full registratio      %3.3e
+    gradient descent      %3.3e\n",  options.regularizerWeight * regularizerCoarse,  options.regularizerWeight * regularizerFine,0)
+
+    @printf("\n
+    J values
+    ---------------------------------
+    single Grid           %3.3e
+    full registratio      %3.3e
+    gradient descent      %3.3e\n", ssdCoarse + options.regularizerWeight * regularizerCoarse, ssdFine + options.regularizerWeight * regularizerFine,0)
+
+
+    return singleGridDisplacement, fineDisplacement
+
+end
+
+initialDisplacement, fineDisplacement = testFixedGridRegistration(plot=true, showDeformationImage=true)
---
+++ experimental/twoResolutionRegistration.jl
@@ -0,0 +1,164 @@
+using Images
+using Logging
+
+using DeformableRegistration: ImageProcessing, Transformation, Interpolation, Distance, Regularizer, Optimization
+using DeformableRegistration.regOptions
+using Interpolations
+import DeformableRegistration.Interpolation.interpolateArray
+include("../src/helpers/objectiveFunctionCreation.jl")
+include("./constraint.jl")
+
+function registerNonParametricOnTargetGrid(referenceImage, templateImage, targetGrid::scaledArray,
+                                     options::regOptions;
+                                     affineParameters = [1.0,0,0,0,1.0,0],
+                                     measureDistance = ssdDistanceArbitraryGrid,
+                                     regularizerOperator=createCurvatureOperatorCentered,
+                                     initialDisplacement=scaledArray(zeros(1),(1,),[],[]),
+                                     constraint = 0,
+                                     interpolationScheme=InterpLinearFast,
+                                     gradientDescentOnly=false)#BSpline(Linear()))
+
+  doConstraints = constraint != 0
+
+  affineParametersInitial = affineParameters
+  options.parametricOnly = false
+  referenceGrid = []; deformedGrid = []; imageSize = [];
+  # R_level1 = restrictResolutionToLevel(referenceImage, gridLevel)
+  # T_level1 = restrictResolutionToLevel(templateImage,  gridLevel)
+  # gridVoxelSize = R_level1.voxelsize
+  regularizerMatrix = regularizerOperator(targetGrid.voxelsize, t2a(targetGrid.dimensions))
+  centeredGrid = targetGrid;
+
+  if initialDisplacement.data == zeros(1)
+      referenceGrid = transformGridAffine(centeredGrid,affineParameters)
+      deformedGrid = referenceGrid
+  else
+      referenceGrid = transformGridAffine(centeredGrid,affineParameters)
+      deformedGrid = interpolateDeformationField(initialDisplacement, referenceGrid, interpolationScheme=interpolationScheme) + referenceGrid
+  end
+
+
+  # start multilevel registration
+  for level = options.levels
+
+  	R = restrictResolutionToLevel(referenceImage,level)
+  	T = restrictResolutionToLevel(templateImage,level)
+    centeredGrid = getCellCenteredGrid(R)
+
+  	imageSize = getSize(R)
+  	Logging.info("level ",level,": [",size(R.data)[1],"]x[",size(R.data)[2],"]")
+
+  	# define objective function
+  	Jfunc(grid;doDerivative=false,doHessian=false) =
+        measureDistance(R, T, scaledArray(grid, targetGrid.dimensions,
+                targetGrid.voxelsize, targetGrid.shift),
+            doDerivative=doDerivative, doHessian=doHessian, options=options)[1:3] +
+        options.regularizerWeight * regularizer(grid-referenceGrid.data,regularizerMatrix)
+    fValues(grid) = [
+        measureDistance(R, T, scaledArray(grid, targetGrid.dimensions,
+                targetGrid.voxelsize, targetGrid.shift),
+            doDerivative=false, doHessian=false, options=options)[1:3],
+        options.regularizerWeight , regularizer(grid-referenceGrid.data,regularizerMatrix)]
+  	## gauss newton method
+    if doConstraints
+        initialDisplacementCoarse = interpolateDeformationField(initialDisplacement, referenceGrid, interpolationScheme=interpolationScheme) + referenceGrid
+        c(x) = constraint(x, initialDisplacementCoarse)
+        deformedGrid = opt(Jfunc, deformedGrid.data, referenceGrid.data, options, constraint= c, printFunction = fValues, gradientDescentOnly=gradientDescentOnly)
+    else
+        deformedGrid = opt(Jfunc, deformedGrid.data, referenceGrid.data, options, printFunction = fValues, gradientDescentOnly=gradientDescentOnly)
+    end
+    deformedGrid = scaledArray(deformedGrid, targetGrid.dimensions, targetGrid.voxelsize, targetGrid.shift)
+  end
+
+  # return deformation field
+  displacementField = deformedGrid-referenceGrid
+  if options.interpolateToReferenceImage
+      referenceGrid = transformGridAffine(getCellCenteredGrid(referenceImage),affineParameters)
+      return interpolateDeformationField(displacementField, referenceGrid, interpolationScheme=interpolationScheme)
+  else
+      return displacementField
+  end
+
+end
+
+
+function registerInTwoResolutions(referencePatch::regImage, templatePatch::regImage,
+                                     referenceImage::regImage, templateImage::regImage,
+                                     targetGrid::scaledArray,
+                                     options::regOptions;
+                                     affineParameters = [1.0,0,0,0,1.0,0],
+                                     measureDistance = ssdDistanceArbitraryGrid,
+                                     regularizerOperator=createCurvatureOperatorCentered,
+                                     initialDisplacement=scaledArray(zeros(1),(1,),[],[]),
+                                     constraint = 0,
+                                     interpolationScheme=InterpLinearFast,
+                                     gradientDescentOnly=false,
+                                     patchWeight = 1.0,
+                                     imageWeight = 1.0,
+                                     imageLevel = 4,
+                                     patchLevel = 0)#BSpline(Linear()))
+
+  doConstraints = constraint != 0
+  affineParametersInitial = affineParameters
+  options.parametricOnly = false
+
+  referenceGrid = []; deformedGrid = []; imageSize = [];
+  regularizerMatrix = regularizerOperator(targetGrid.voxelsize, t2a(targetGrid.dimensions))
+  centeredGrid = targetGrid;
+
+  if initialDisplacement.data == zeros(1)
+      referenceGrid = transformGridAffine(centeredGrid,affineParameters)
+      deformedGrid = referenceGrid
+  else
+      referenceGrid = transformGridAffine(centeredGrid,affineParameters)
+      deformedGrid = interpolateDeformationField(initialDisplacement, referenceGrid, interpolationScheme=interpolationScheme) + referenceGrid
+  end
+
+## for level loop
+  # start multilevel registration
+
+    R = referenceImage
+    T = templateImage
+    centeredGrid = getCellCenteredGrid(R)
+
+  	imageSize = getSize(R)
+  	Logging.info("imageLevel ",imageLevel,": [",size(R.data)[1],"]x[",size(R.data)[2],"]")
+
+  	# define objective function
+  	Jfunc(grid;doDerivative=false,doHessian=false) =
+            imageWeight * measureDistance(R, T,
+                scaledArray(grid, targetGrid.dimensions,
+                targetGrid.voxelsize, targetGrid.shift), doDerivative=doDerivative,
+                doHessian=doHessian, options=options, centeredGrid=centeredGrid)[1:3] +
+            patchWeight * measureDistance(referencePatch, templatePatch,
+                    scaledArray(grid, targetGrid.dimensions,
+                    targetGrid.voxelsize, targetGrid.shift), doDerivative=doDerivative,
+                    doHessian=doHessian, options=options, centeredGrid=centeredGrid)[1:3] +
+            options.regularizerWeight * regularizer(grid-referenceGrid.data,regularizerMatrix)
+
+    fValues(grid) = [measureDistance(R, T,
+                scaledArray(grid, targetGrid.dimensions,
+                targetGrid.voxelsize, targetGrid.shift), doDerivative=false,
+                doHessian=false, options=options, centeredGrid=centeredGrid)[1:3],
+            patchWeight * measureDistance(referencePatch, templatePatch,
+                    scaledArray(grid, targetGrid.dimensions,
+                    targetGrid.voxelsize, targetGrid.shift), doDerivative=false,
+                    doHessian=false, options=options, centeredGrid=centeredGrid)[1:3],
+            options.regularizerWeight , regularizer(grid-referenceGrid.data,regularizerMatrix)]
+  	## gauss newton method
+    if doConstraints
+        initialDisplacementCoarse = interpolateDeformationField(initialDisplacement, referenceGrid, interpolationScheme=interpolationScheme) + referenceGrid
+        c(x) = constraint(x, initialDisplacementCoarse)
+        deformedGrid = opt(Jfunc, deformedGrid.data, referenceGrid.data, options, constraint= c, printFunction = fValues, gradientDescentOnly=gradientDescentOnly)
+    else
+        deformedGrid = opt(Jfunc, deformedGrid.data, referenceGrid.data, options, printFunction = fValues, gradientDescentOnly=gradientDescentOnly)
+    end
+    deformedGrid = scaledArray(deformedGrid, targetGrid.dimensions,
+                        targetGrid.voxelsize, targetGrid.shift)
+  ## end level loop
+
+  # return deformation field
+  displacementField = deformedGrid-referenceGrid
+  return displacementField
+
+end
